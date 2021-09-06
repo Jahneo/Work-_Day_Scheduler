ba0928f8eeaae28a62c087e335af0c86d68a7240
Jahneo <85087102+Jahneo@users.noreply.github.com>
2021-09-06 03:49:05 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,18 +59,7 @@ var todayDate = moment().format('dddd, MMM Do YYYY');
  for (int #hour = 9; #hour < 18; #hour ++){
      $("#hour .description").val(localStorage.getItem("hour"));
     }
-     /*
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
-    $("#hour18 .description").val(localStorage.getItem("hour18"));
-    */
+   
     keepTrackONTime();
 })
 
